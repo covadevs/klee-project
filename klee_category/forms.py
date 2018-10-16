192fd73ce63a06e836cda4ab3139ae6b6dfb03d3
wellysondannilo <wellyson.dannilo@dce.ufpb.br>
2018-10-16 13:35:31 -0300

    placeholder e max_length no cadastro de categoria

--- klee_category/forms.py
+++ klee_category/forms.py
@@ -3,6 +3,13 @@ from django.forms import ModelForm
 from .models import Category
 
 class CreateCategoryForm(ModelForm):
+    category_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Category name'
+    }))
+    category_acron = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Acron',
+        'maxlength': '2'
+    }))
     class Meta:
         model=Category
         fields = ['category_name', 'category_acron']
